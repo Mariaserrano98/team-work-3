55690eb3b81509d9986d21dd3eb4793107a8293f
maria <serranoortizmaria020@gmail.com>
2023-04-19 16:06:18 +0200

    algunos cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
 console.log('Hello World!');
+
+const menuIcon = document.querySelector(".menu-icon");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close");
+
+menuIcon.addEventListener("click", function() {
+  modal.style.display = "block";
+  menuIcon.style.display = "none";
+});
+
+closeBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+  menuIcon.style.display = "block";
+});
